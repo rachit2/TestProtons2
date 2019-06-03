cdfcdd7d02a65427831f40b6773cb026bad4a7cf
rachit2 <rachitverma.001@gmail.com>
2019-06-03 19:32:58 +0530

    Update JsonData.rb

--- JsonData.rb
+++ JsonData.rb
@@ -1,10 +1,11 @@
 require 'net/http'
 require 'json'
+#require 'benchmark/ips'
 class A
-	def self.score
-		result = Net::HTTP.get(URI.parse('https://api.github.com/users/dhh/events/public'))
-		dhh_score = 0 
-		score_card = {
+	def initialize
+		@result = Net::HTTP.get(URI.parse('https://api.github.com/users/dhh/events/public'))
+		@dhh_score = 0 
+		@score_card = {
 		"IssuesEvent" => 7,
 		"IssueCommentEvent" => 6,
 		"PushEvent" => 5,
@@ -13,11 +14,11 @@ class A
 		"CreateEvent" => 2,
 		"Any other event" => 1 
 		}
-		(JSON.parse(result).group_by{|obj| obj["type"]}.map {|k,v| [k, v.length]}).each do |record|
-			value = score_card.fetch(record[0]) rescue score_card.fetch("Any other event")
-		  dhh_score += value * record[1]
-  	end
-		puts "DHH's github score is #{dhh_score}"
+	end
+	def calScore
+		 JSON.parse(@result).each{|obj| @dhh_score+=@score_card.fetch(obj["type"],1) }
+		return @dhh_score
 	end
 end
-A.score
+b=A.new
+puts b.calScore
